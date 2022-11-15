19f4ca3d4b04cabc69dc867158fabbceae1a7fce
TianYuping <tianyuping@zhihu.com>
2022-11-15 17:34:50 +0800

    卡车最大载货

---
+++ leetcode/贪心算法/卡车最大载货.go
@@ -0,0 +1,53 @@
+package 贪心算法
+
+import "sort"
+
+/*
+请你将一些箱子装在 一辆卡车 上。给你一个二维数组 boxTypes ，其中 boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi] ：
+
+numberOfBoxesi 是类型 i 的箱子的数量。
+numberOfUnitsPerBoxi 是类型 i 每个箱子可以装载的单元数量。
+整数 truckSize 表示卡车上可以装载 箱子 的 最大数量 。只要箱子数量不超过 truckSize ，你就可以选择任意箱子装到卡车上。
+
+返回卡车可以装载 单元 的 最大 总数。
+
+
+
+示例 1：
+
+输入：boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
+输出：8
+解释：箱子的情况如下：
+- 1 个第一类的箱子，里面含 3 个单元。
+- 2 个第二类的箱子，每个里面含 2 个单元。
+- 3 个第三类的箱子，每个里面含 1 个单元。
+可以选择第一类和第二类的所有箱子，以及第三类的一个箱子。
+单元总数 = (1 * 3) + (2 * 2) + (1 * 1) = 8
+示例 2：
+
+输入：boxTypes = [[5,10],[2,5],[4,7],[3,9]], truckSize = 10
+输出：91
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/maximum-units-on-a-truck
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func maximumUnits(boxTypes [][]int, truckSize int) int {
+	//按箱子内的单元数降序排列
+	sort.Slice(boxTypes, func(i, j int) bool {
+		return boxTypes[i][1] > boxTypes[j][1]
+	})
+
+	var res int
+	for _, b := range boxTypes {
+		//当卡车剩余格子放不下全部箱子，只放剩余格子数量
+		if b[0] >= truckSize {
+			res += truckSize * b[1]
+			break
+		}
+		res += b[0] * b[1]
+		truckSize -= b[0]
+	}
+	return res
+}
