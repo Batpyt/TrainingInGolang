5cbb3b85a05df75e27cc0f5e303f77d0ff019cb7
tianyuping01 <Batpyt@users.noreply.github.com>
2023-01-02 22:52:14 +0800

    二叉树镜像&对称二叉树

---
+++ leetcode/tree/二叉树的镜像.go
@@ -0,0 +1,16 @@
+package tree
+
+/*
+输出二叉树镜像
+一层层左右互换递归
+*/
+func mirrorTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	left := mirrorTree(root.Left)
+	right := mirrorTree(root.Right)
+	root.Left = right
+	root.Right = left
+	return root
+}
