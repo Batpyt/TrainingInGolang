b8959d4cb3a04a3b2873d4b7148cff74a18361dd
tianyuping01 <Batpyt@users.noreply.github.com>
2023-01-02 18:09:28 +0800

    合并排序链表

---
+++ leetcode/链表/合并排序链表.go
@@ -0,0 +1,24 @@
+package 链表
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	res := &ListNode{}
+	head := res
+
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			head.Next = l1
+			l1 = l1.Next
+		} else {
+			head.Next = l2
+			l2 = l2.Next
+		}
+		head = head.Next
+	}
+	if l1 != nil {
+		head.Next = l1
+	}
+	if l2 != nil {
+		head.Next = l2
+	}
+	return res.Next
+}
