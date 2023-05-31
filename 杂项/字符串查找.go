f37070a8a8926dd36f869d95e1bdf2f1879b910d
tianyuping01 <Batpyt@users.noreply.github.com>
2023-05-31 21:19:05 +0800

    字符串查找

---
+++ 杂项/字符串查找.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+/*
+helloworld
+     world
+*/
+
+func main() {
+	fmt.Println(findStr("helloworld", "world"))
+	fmt.Println(findStr("helloworld", "llo"))
+	fmt.Println(findStr("helloworld", "iii"))
+}
+
+func findStr(str, target string) int {
+	if len(target) > len(str) {
+		return -1
+	}
+
+	left, right := 0, len(target)-1
+	for right < len(str) {
+
+		temp := str[left : right+1]
+		//fmt.Println(temp)
+		if temp == target {
+			return left
+		}
+		left++
+		right++
+	}
+	return -1
+}
