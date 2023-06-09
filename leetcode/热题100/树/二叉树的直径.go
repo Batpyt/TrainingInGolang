394831f8d7416c347330220d4249babb7161d33b
tianyuping <1479012386@qq.com>
2023-06-09 16:25:43 +0800

    二叉树的直径

---
+++ leetcode/热题100/树/二叉树的直径.go
@@ -0,0 +1,45 @@
+package 树
+
+/*
+给你一棵二叉树的根节点，返回该树的 直径 。
+
+二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径可能经过也可能不经过根节点 root 。
+
+两节点之间路径的 长度 由它们之间边数表示。
+
+示例 1：
+
+输入：root = [1,2,3,4,5]
+输出：3
+解释：3 ，取路径 [4,2,1,3] 或 [5,2,1,3] 的长度。
+示例 2：
+
+输入：root = [1,2]
+输出：1
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/diameter-of-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func diameterOfBinaryTree(root *TreeNode) int {
+	res := 0
+	var dfs func(node *TreeNode) int
+	dfs = func(node *TreeNode) int {
+		if node == nil {
+			return -1 // 下面 +1 后，对于叶子节点就刚好是 0
+		}
+		lLen := dfs(node.Left) + 1  // 左子树最大链长+1
+		rLen := dfs(node.Right) + 1 // 右子树最大链长+1
+		res = max(res, lLen+rLen)   // 两条链拼成路径
+		return max(lLen, rLen)      // 当前子树最大链长
+	}
+	dfs(root)
+	return res
+}
+
+//func max(a, b int) int {
+//	if a > b {
+//		return a
+//	}
+//	return b
+//}
