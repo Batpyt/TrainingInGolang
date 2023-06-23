cf54c7be8a63b46767c4696ca2872e6927e8d174
tianyuping <1479012386@qq.com>
2023-06-23 21:50:28 +0800

    最小栈

---
+++ leetcode/热题100/栈/最小栈.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/*
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+实现 MinStack 类:
+
+MinStack() 初始化堆栈对象。
+void push(int val) 将元素val推入堆栈。
+void pop() 删除堆栈顶部的元素。
+int top() 获取堆栈顶部的元素。
+int getMin() 获取堆栈中的最小元素。
+
+
+示例 1:
+
+输入：
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+输出：
+[null,null,null,null,-3,null,0,-2]
+
+解释：
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/min-stack
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func main() {
+	ms := Constructor()
+	ms.Push(-2)
+	ms.Push(0)
+	ms.Push(-3)
+	fmt.Println(ms.stack, ms.minStack)
+	fmt.Println(ms.GetMin())
+	ms.Pop()
+	fmt.Println(ms.stack, ms.minStack)
+	fmt.Println(ms.GetMin())
+
+}
+
+/*
+维护一个辅助栈，辅助栈永远和主体栈length相同，存放的是到目前为止push进来的最小的元素
+*/
+
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+func Constructor() MinStack {
+	return MinStack{
+		stack:    []int{},
+		minStack: []int{math.MaxInt64},
+	}
+}
+
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+	//辅助栈将对比新push的元素和之前元素，进入更小的那个
+	this.minStack = append(this.minStack, min(this.minStack[len(this.minStack)-1], val))
+}
+
+func (this *MinStack) Pop() {
+	this.stack = this.stack[:len(this.stack)-1]
+	this.minStack = this.minStack[:len(this.minStack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack)-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
